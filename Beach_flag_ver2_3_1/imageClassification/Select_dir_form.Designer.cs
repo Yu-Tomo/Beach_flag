bf10fe7b5f3421f5ba69f100e9f1fd95559e3fd9
Yuto-Tomono <59984707+Yuto-Tomono@users.noreply.github.com>
2021-01-26 19:03:56 +0900

    version 2.3.1

---
+++ Beach_flag_ver2_3_1/imageClassification/Select_dir_form.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace imageClassification
+{
+    partial class Select_dir_form
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.folderPath = new System.Windows.Forms.TextBox();
+            this.miss_f = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.end_button = new System.Windows.Forms.Button();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // miss_t
+            // 
+            this.folderPath.AllowDrop = true;
+            this.folderPath.Location = new System.Drawing.Point(85, 33);
+            this.folderPath.Name = "miss_t";
+            this.folderPath.Size = new System.Drawing.Size(372, 19);
+            this.folderPath.TabIndex = 6;
+            this.folderPath.DragDrop += new System.Windows.Forms.DragEventHandler(this.folderDrop);
+            this.folderPath.DragEnter += new System.Windows.Forms.DragEventHandler(this.folderDragEntry);
+            // 
+            // miss_f
+            // 
+            this.miss_f.Location = new System.Drawing.Point(12, 33);
+            this.miss_f.Name = "miss_f";
+            this.miss_f.Size = new System.Drawing.Size(54, 19);
+            this.miss_f.TabIndex = 5;
+            this.miss_f.Text = "開く";
+            this.miss_f.UseVisualStyleBackColor = true;
+            this.miss_f.Click += new System.EventHandler(this.miss_f_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(15, 45);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 12);
+            this.label1.TabIndex = 7;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.folderPath);
+            this.groupBox2.Controls.Add(this.miss_f);
+            this.groupBox2.Location = new System.Drawing.Point(12, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(473, 75);
+            this.groupBox2.TabIndex = 10;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "保存先のディレクトリ";
+            this.groupBox2.Enter += new System.EventHandler(this.groupBox2_Enter);
+            // 
+            // end_button
+            // 
+            this.end_button.Location = new System.Drawing.Point(362, 93);
+            this.end_button.Name = "end_button";
+            this.end_button.Size = new System.Drawing.Size(123, 29);
+            this.end_button.TabIndex = 7;
+            this.end_button.Text = "保存する";
+            this.end_button.UseVisualStyleBackColor = true;
+            this.end_button.Click += new System.EventHandler(this.end_button_Click);
+            // 
+            // Select_dir_form
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(497, 132);
+            this.Controls.Add(this.end_button);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.groupBox2);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Select_dir_form";
+            this.Text = "保存先の選択";
+            this.DragDrop += new System.Windows.Forms.DragEventHandler(this.folderDrop);
+            this.DragEnter += new System.Windows.Forms.DragEventHandler(this.folderDragEntry);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox folderPath;
+        private System.Windows.Forms.Button miss_f;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button end_button;
+    }
+}
---
+++ Beach_flag_ver2_3_1/imageClassification/Select_dir_form.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace imageClassification
+{
+    public partial class Select_dir_form : Form
+    {
+        public mainForm MainForm;
+        public Select_dir_form()
+        {
+            InitializeComponent();
+        }
+        private string dirPath;
+        private string saveDir;
+      
+        public string DirPath { get { return dirPath; } set { dirPath = value; } }
+        private string dirOpen(TextBox text)
+        {
+            string str;
+            if (text.Text == "")
+            {
+                FolderOpen folderOpen = new FolderOpen();
+                str=folderOpen.FolderOpenDia(this) + Path.DirectorySeparatorChar;
+                folderPath.Text = str;
+            }
+            else
+            {
+                str = text.Text;
+            }
+            return str;
+        }
+        private void miss_f_Click(object sender, EventArgs e)
+        {
+            saveDir= dirOpen(folderPath);                                   
+        }
+
+        private void end_button_Click(object sender, EventArgs e)
+        {
+            
+            MainForm.SavePath = saveDir;
+           
+            if(saveDir == null)MessageBox.Show("ディレクトリを選択してください。", "エラー",MessageBoxButtons.OK,MessageBoxIcon.Error);
+          
+            this.Close();
+        }
+        private void groupBox2_Enter(object sender, EventArgs e)
+        {
+
+        }
+        private void folderDragEntry(object sender, DragEventArgs e)
+        {
+
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                e.Effect = DragDropEffects.All;
+            }
+            else
+            {
+                e.Effect = DragDropEffects.None;
+            }
+        }
+        private void folderDrop(object sender, DragEventArgs e)
+        {
+            string[] str;
+
+            str = (string[])e.Data.GetData(DataFormats.FileDrop, false);
+            if (str.Length > 1) MessageBox.Show("複数ファイル選択には対応していません。", "注意", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+
+            if (File.Exists(str[0]))
+            {
+                MessageBox.Show("フォルダーを選択してください。", "エラー", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                str[0] += Path.DirectorySeparatorChar;
+                folderPath.Text = str[0];
+                saveDir = str[0];
+            }
+        }
+    }
+}
